93df6041c16f0608123728144006bb66a067af23
AntMidd420 <30540505+AntMidd420@users.noreply.github.com>
2017-11-22 08:19:23 -0600

    Create index.html

---
+++ index.js
@@ -0,0 +1,8 @@
+var dayOfWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+var listItem = ['Milk','eggs'.'bacon','ground hamburger','hamburger buns', 'hot dogs','hot dog buns','ketchup','mustard','chocalate chips','condensed milk','bread'];
+var useItem = listItem[] - dayOfWeek[];
+var useItems = function() {
+	if() 
+
+}
